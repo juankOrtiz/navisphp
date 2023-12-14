8c07fd13b89c770cfb0ba15f583717c9a8f5e252
juankOrtiz <juank.ortiz.1991@gmail.com>
2023-12-14 01:08:11 -0300

    Agregar validación de cadena opcional y número

--- Core/Validator.php
+++ Core/Validator.php
@@ -11,7 +11,21 @@ class Validator
         return strlen($value) >= $min && strlen($value) <= $max;
     }
 
+    public static function optionalString($value, $min = 1, $max = INF): bool
+    {
+        if ($value === null || $value === '') {
+            return true;
+        }
+
+        return self::string($value, $min, $max);
+    }
+
     public static function email($value) {
         return filter_var($value, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function number($value, $min = 1, $max = INF): bool
+    {
+        return is_numeric($value) && (int)$value >= $min && (int)$value <= $max;
     }
 }
