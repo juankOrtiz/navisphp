1c5f642024dd1d6a40786d0dca5dfe0648fd9fbc
juankOrtiz <juank.ortiz.1991@gmail.com>
2023-12-14 10:58:54 -0300

    Agregar mapeo de funciones del router

--- Core/Router.php
+++ Core/Router.php
@@ -6,10 +6,12 @@ use Core\Middleware\Auth;
 use Core\Middleware\Guest;
 use Core\Middleware\Middleware;
 
-class Router {
+class Router
+{
   protected array $routes = [];
 
-  public function add(string $method, string $uri, string $controller): self {
+  public function add(string $method, string $uri, string $controller): self
+  {
     $this->routes[] = [
       'uri' => $uri,
       'controller' => $controller,
@@ -20,52 +22,47 @@ class Router {
     return $this;
   }
 
-  public function get(string $uri, string $controller): self {
-    return $this->add('GET', $uri, $controller);
+  // Función que permite responder al llamado de las funciones que tengan el nombre de los métodos HTTP soportados.
+  public function __call(string $name, array $args): self
+  {
+    $httpMethods = ['get', 'post', 'delete', 'patch', 'put'];
+
+    if (in_array($name, $httpMethods)) {
+      return $this->add(strtoupper($name), ...$args);
+    } else {
+      throw new \BadMethodCallException("Method {$name} not supported.");
+    }
   }
 
-  public function post(string $uri, string $controller): self {
-    return $this->add('POST', $uri, $controller);
-  }
-
-  public function delete(string $uri, string $controller): self {
-    return $this->add('DELETE', $uri, $controller);
-  }
-
-  public function patch(string $uri, string $controller): self {
-    return $this->add('PATCH', $uri, $controller);
-  }
-
-  public function put(string $uri, string $controller): self {
-    return $this->add('PUT', $uri, $controller);
-  }
-
-  public function only(string $key): self {
+  public function only(string $key): self
+  {
     $this->routes[array_key_last($this->routes)]['middleware'] = $key;
 
     return $this;
   }
 
-  public function route(string $uri, string $method) {
+  public function route(string $uri, string $method)
+  {
     foreach ($this->routes as $route) {
-      if($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+      if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
         Middleware::resolve($route['middleware']);
 
-        return require base_path('Http/controllers' .$route['controller']);
+        return require base_path('Http/controllers' . $route['controller']);
       }
     }
 
     $this->abort();
   }
 
-  public function previousUrl(): string {
-      return $_SERVER['HTTP_REFERER'];
+  public function previousUrl(): string
+  {
+    return $_SERVER['HTTP_REFERER'];
   }
 
   protected function abort(int $code = 404): void
   {
-      http_response_code($code);
-      require base_path("views/{$code}.php");
-      die();
+    http_response_code($code);
+    require base_path("views/{$code}.php");
+    die();
   }
 }
