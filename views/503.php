635415ca74d4607d74120b01159c2ba4885fa1ea
juankOrtiz <juank.ortiz.1991@gmail.com>
2023-12-14 17:45:04 -0300

    Crear vista para el modo mantenimiento

---
+++ views/503.php
@@ -0,0 +1,13 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+
+<main>
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <h1 class="text-2xl font-bold">El sitio se encuentra actualmente en mantenimiento</h1>
+        <p class="mt-4">
+            <a href="/" class="text-blue underline">Volver al inicio</a>
+        </p>
+    </div>
+</main>
+
+<?php require('partials/footer.php') ?>
